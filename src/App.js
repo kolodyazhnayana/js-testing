b1429cc3fa9ee69f77eeb9d9943527298f639e81
Yana Kolodyazhnaya <yana.kolodyazhnaya19@gmail.com>
2022-04-20 18:13:29 +0300

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useEffect, useState} from "react"
 
 function App() {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        (async function() {
+            let res = await fetch('https://jsonplaceholder.typicode.com/users')
+            if (res.ok) {
+                let json = await res.json()
+                let filterRes = json.filter(item => item.id % 2 === 0)
+                setUsers(filterRes)
+            } else {
+                console.log(res.status)
+            }
+        })()
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className='container'>
+            <h2>Name, surname, email</h2>
+            {
+                users.map(item => <span key={item.id}>{`${item.name} -> ${item.email}`}</span>)
+            }
+        </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
